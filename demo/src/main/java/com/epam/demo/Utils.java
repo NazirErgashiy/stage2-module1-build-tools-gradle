cb1a1b59b37b80ddd4a674fd7d68132f179f589d
NazirErgashiy <pikobiznes@gmail.com>
2023-02-14 15:56:50 +0500

    Check

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,19 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i < args.size(); i++) {
+            integerList.add(Integer.parseInt(args.get(i)));
+        }
+        for (int i = 0; i < integerList.size(); i++) {
+            if (integerList.get(i) <= 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import java.*;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        int parser = 0;
+        if (Integer.parseInt(str) > 0) {
+            return true;
+        }
+        return false;
     }
 }
